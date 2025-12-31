bca309a5c9ed953e4d083e3dbb63d478e51c89cb
uttam <user@gmail.com>
2025-12-31 18:51:36 +0530

    here is a less modification

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello From Github");
-Console.WriteLine("Hello From Github");
-Console.WriteLine("Hello From Github");
-Console.WriteLine("Hello From Github");
